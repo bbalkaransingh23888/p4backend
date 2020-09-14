5ed788d7a41073eaa5a81a89c8f8e4691d034ed4
Marilyn Balkaransingh <marilynbalkaransingh@marilyns-air.home>
2020-09-14 17:21:42 -0400

    urls files added and completed

---
+++ p4_app/p4_api/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from rest_framework import routers
+from p4_app.p4_api.views import CategoryViewSet, CategoryGames, SingleCategoryGame, GameViewSet
+
+router = routers.DefaultRouter()
+router.register('categories', CategoryViewSet, basename='categories')
+router.register('recipes', GameViewSet, basename='recipes')
+
+custom_urlpatterns = [
+   url(r'categories/(?P<category_pk>\d+)/games$', CategoryGames.as_view(), name='category_recipes'),
+   url(r'categories/(?P<category_pk>\d+)/games/(?P<pk>\d+)$', SingleCategoryGame.as_view(),
+       name='single_category_game'),
+   ]
+
+urlpatterns = router.urls
+urlpatterns += custom_urlpatterns
