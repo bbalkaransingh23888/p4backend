760d4e65ce1407675f7b697d2ef11e528dff81cb
Marilyn Balkaransingh <marilynbalkaransingh@marilyns-air.home>
2020-11-11 22:52:01 -0500

    added back owner field

--- p4_app/p4_api/models.py
+++ p4_app/p4_api/models.py
@@ -20,7 +20,7 @@ class Game(models.Model):
     class Meta:
         verbose_name_plural = 'games'
 
-    # owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     category = models.ForeignKey(Category, related_name='categories', on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=100)
     image_url = models.URLField(blank=True)
